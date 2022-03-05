73d28d203b06faffd429ae01b85d324e8b75001b
punitjain-jp <76389204+punitjain-jp@users.noreply.github.com>
2022-03-05 08:22:47 +0530

    Update fda_app.py

--- fda_app.py
+++ fda_app.py
@@ -10,7 +10,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <a class="navbar-brand" href="#">Project-III</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
